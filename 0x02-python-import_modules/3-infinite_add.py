a98496da42d3d0bf8b3044ef490e5a54b8681542
IdtDanny <idtdanny@gmail.com>
2023-06-08 17:35:46 +0000

    3. Infinite addition

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    result = 0
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0")
+    else:
+        for i in range(count):
+            result += int(sys.argv[i + 1])
+        print("{}".format(result))
