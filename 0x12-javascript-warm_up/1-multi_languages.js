c8c5ccfc80ec7d5ca0ea0214ef04eced01b92718
IdtDanny <idtdanny@gmail.com>
2023-09-11 12:41:08 -0700

    Done up to fifth

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,5 +3,5 @@ const firstLine = 'C is fun';
 const secondLine = 'Python is cool';
 const thirdLine = 'JavaScript is amazing';
 console.log(firstLine);
-console.log(secndLine);
+console.log(secondLine);
 console.log(thirdLine);
