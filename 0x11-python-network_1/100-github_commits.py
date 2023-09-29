f5e2d54aad51e021ed85057caa1e376d0e83790e
IdtDanny <idtdanny@gmail.com>
2023-09-29 14:01:06 -0700

    all challenges done

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Python script that takes 2 arguments in order to solve this challenge.
+"""
+
+
+if __name__ == "__main__":
+    from requests import get
+    from sys import argv
+
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(argv[2], argv[1])
+    commits = get(url).json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
